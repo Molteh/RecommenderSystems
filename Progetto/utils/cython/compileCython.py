3c1a5dfa9701fd0823cbee87e233959ac167f246
iPhra <francesco1.lorenzo@mail.polimi.it>
2018-11-30 22:02:09 +0100

    Fixes

--- Progetto/utils/cython/compileCython.py
+++ Progetto/utils/cython/compileCython.py
@@ -2,16 +2,48 @@
 # -*- coding: utf-8 -*-
 """
 Created on 16/07/2017
-
 @author: Maurizio Ferrari Dacrema
 """
 
 
-from distutils.core import setup
-from Cython.Build import cythonize
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
+
+
+from Cython.Distutils import build_ext
+
+
 import numpy
 
+import sys
+import re
+
+
+if len(sys.argv) != 4:
+    raise ValueError("Wrong number of paramethers received. Expected 4, got {}".format(sys.argv))
+
+
+#fileToCompile = 'SLIM_ElasticNet_Cython_Epoch.pyx'
+
+# Get the name of the file to compile
+fileToCompile = sys.argv[1]
+# Remove the argument from sys argv in order for it to contain only what setup needs
+del sys.argv[1]
+
+extensionName = re.sub("\.pyx", "", fileToCompile)
+
+
+ext_modules = Extension(extensionName,
+                [fileToCompile],
+                extra_compile_args=['-O3'],
+                include_dirs=[numpy.get_include(),],
+                )
+
 setup(
-    ext_modules = cythonize("Compute_Similarity_Cython.pyx"),
-    include_dirs=[numpy.get_include()]
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[ext_modules]
 )
